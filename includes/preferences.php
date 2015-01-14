29bf52d90fd06d0d8c6827ff94b5330f158b0022
bakerj32 <jordan.tod.baker@gmail.com>
2015-01-14 12:23:19 -0800

    Update preferences.php

--- includes/preferences.php
+++ includes/preferences.php
@@ -1,7 +1,6 @@
 <?php
 	$server = 'localhost';
-	$db = 'poemods_mods';
-	$dbUsername = 'poemods_chode';
-	$dbPassword = '6H,{%4pk{;oC';
-	$steam_key = 'B1E4BF969B848CC0C379FA0962069EF0';
+	$db = '';
+	$dbUsername = '';
+	$dbPassword = '';
 ?>
